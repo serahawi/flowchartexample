4569c46abfcb1a340f1927aa40a8aa7688b0906d
Sara Ashraf <119938202+serahawi@users.noreply.github.com>
2023-02-23 19:34:11 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-network-graph :nodes="nodes" :edges="edges" />
+</template>
+
+<script>
+
+export default {
+  name: "App",
+    data () {return {
+    nodes,
+    edges
+  }}
+};
+
+const nodes = {
+  node1: { name: "Node 1" },
+  node2: { name: "Node 2" },
+  node3: { name: "Node 3" },
+  node4: { name: "Node 4" },
+}
+
+const edges = {
+  edge1: { source: "node1", target: "node2" },
+  edge2: { source: "node2", target: "node3" },
+  edge3: { source: "node3", target: "node4" },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import VNetworkGraph from "v-network-graph";
+import "v-network-graph/lib/style.css";
+const app = createApp(App);
+app.use(VNetworkGraph);
+app.mount("#app");
